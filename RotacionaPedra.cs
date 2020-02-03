88d0c593f42367220f26c9935768f1d3d074c102
pandora1431 <45410959+pandora1431@users.noreply.github.com>
2020-02-03 04:55:44 -0300

    Add files via upload

---
+++ RotacionaPedra.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotacionaPedra : MonoBehaviour {
+	
+	void Update () {
+		transform.Rotate(new Vector3(0, 0, 1), 30 * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Player")||(other.gameObject.CompareTag("tiro")))
+			{
+			Destroy(gameObject);
+			}
+	}
+}
