824f8e689a905cb11d3df19fd8a4ab7d14ec19f9
pandora1431 <45410959+pandora1431@users.noreply.github.com>
2020-02-06 20:09:02 -0300

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	public float MoveSpeed;
+	public float RotationSpeed;
+	CharacterController cc;
+	private Animator anim;
+	protected Vector3 gravidade = Vector3.zero;
+	protected Vector3 move = Vector3.zero;
+	private bool jump;
+    public Joystick Joystick;
+    public botaoscript botaoscript;
+
+
+	
+	void Start()
+	{
+		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator>();
+		anim.SetTrigger("Parado");
+	}
+
+	void Update()
+	{
+
+		Vector3 move = Joystick.Vertical * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+		transform.Rotate (new Vector3 (0, Joystick.Horizontal * RotationSpeed * Time.deltaTime, 0));
+		
+		if (!cc.isGrounded) {
+			gravidade += Physics.gravity * Time.deltaTime;
+		} 
+		else 
+		{
+			gravidade = Vector3.zero;
+			if(jump && !botaoscript.pressed)
+			{
+				gravidade.y = 6f;
+				jump = false;
+			}
+		}
+		move += gravidade;
+		cc.Move (move* Time.deltaTime);
+		Anima ();
+	}
+	 
+	void Anima()
+	{
+		if (!Input.anyKey)
+		{
+			anim.SetTrigger("Parado");
+		} 
+		else 
+		{
+			if(!jump && botaoscript.pressed)
+			{
+				anim.SetTrigger("Pula");
+				jump = true;
+			}
+			else
+			{
+				anim.SetTrigger("Corre");
+			}
+		}
+	}
+}
---
+++ loadscene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Player") 
+        {
+            morreu();
+        }
+    }
+    void morreu() 
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentScene);
+    }
+}
